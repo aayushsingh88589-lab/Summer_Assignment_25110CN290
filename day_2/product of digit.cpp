328d66c2cf54d8d4cdec99deba29b19db7e8ee66
Ayush Singh <aayushsingh88589@gmail.com>
2026-06-04 10:53:11 +0530

    Refactor digit product calculation in C++
    Refactor product calculation and improve user prompts.

--- day_2/product of digit.cpp
+++ day_2/product of digit.cpp
@@ -1,13 +1,20 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 int main() {
-    int num, product = 1;
-    cin >> num;
-    while(num != 0) {
-        product *= num % 10;
-        num /= 10;
-    }
-    cout << product;
-    return 0;
+     int n,product=1;
+     cout << "enter the number: ";
+     cin >> n;
+     int num=n;
+     while(n!=0)
+     
+     {
+        int r=n%10;
+       product=product*r;
+          n=n/10;
+          
+     }
+     cout<<"product of digit is " << product  << endl;
+     
+     return 0;
 }
