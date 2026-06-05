373059e23992a5bcb61374be287856213d22c43d
Ayush Singh <aayushsingh88589@gmail.com>
2026-06-05 20:07:48 +0530

    day 5 completed

--- day_5/Perfect_number.cpp
+++ day_5/Perfect_number.cpp
@@ -9,10 +9,6 @@ int main()
     cout << "Enter a number: ";
     cin >> n;
 
-    /*Find all divisors of the number and add them.
-    
-    If the sum is equal to the number, it is a perfect number.*/
-
     for(int i = 1; i < n; i++)
     {
         if(n % i == 0)
