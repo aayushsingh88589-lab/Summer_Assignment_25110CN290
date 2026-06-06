f3af658092242c655a5dfbf033b4fabeb11f0fa8
Ayush Singh <aayushsingh88589@gmail.com>
2026-06-06 20:30:54 +0530

    day 6 completed

---
+++ day_6/count_bits.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, count = 0;
+    cin >> n;
+    while(n > 0) {
+        count += n & 1;
+        n >>= 1;
+    }
+    cout << count;
+    return 0;
+}
