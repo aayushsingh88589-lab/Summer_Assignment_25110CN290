f82e1cc58610b535cd740fd561c7417f34424366
Ayush Singh <aayushsingh88589@gmail.com>
2026-06-09 14:48:45 +0530

    day 9 completed

---
+++ day_9/repeated_char_pattern.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, j;
+
+    for(i = 0; i <= 4; i++)
+    {
+        for(j = 0; j <= i; j++)
+        {
+            cout << char('A' + i);
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
