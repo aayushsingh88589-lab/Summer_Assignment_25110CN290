2926028b8a06762f030509978e0ba976aaba62c8
Ayush Singh <aayushsingh88589@gmail.com>
2026-06-09 14:52:32 +0530

    day 9 completed

--- day_9/reverse_star.cpp
+++ day_9/reverse_star.cpp
@@ -3,12 +3,8 @@ using namespace std;
 
 int main()
 {
-    int n;
 
-    cout << "Enter number of rows: ";
-    cin >> n;
-
-    for(int i = n; i >= 1; i--)
+    for(int i = 5; i >= 1; i--)
     {
         for(int j = 1; j <= i; j++)
         {
