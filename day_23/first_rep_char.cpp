2546f4836b6cb19e0bd707f654813880a20fddde
Ayush Singh <aayushsingh88589@gmail.com>
2026-06-24 11:22:29 +0530

    day 23 completed

---
+++ day_23/first_rep_char.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char str[200];
+    cout << "Enter a string: ";
+    cin.getline(str, 200);
+
+    int freq[256] = {0};   
+    char result[200];
+    int j = 0;
+
+    // traverse string and keep only first occurrence
+    for(int i = 0; i < strlen(str); i++) {
+        if(freq[(unsigned char)str[i]] == 0) {
+            result[j++] = str[i];   // copy if not seen before
+            freq[(unsigned char)str[i]] = 1;
+        }
+    }
+    result[j] = '\0';   // end result string
+
+    cout << "String without duplicates: " << result;
+    return 0;
+}
